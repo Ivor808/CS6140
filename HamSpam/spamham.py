79a16c77d80007cdc6971aa6a138e7cd721882f2
Ivor <ivor808@gmail.com>
2021-09-18 19:42:59 -0600

    worked on testing

--- HamSpam/spamham.py
+++ HamSpam/spamham.py
@@ -1,31 +1,38 @@
-## for each email we iterate and save each word in respective dictionary spot. Do this on both sets
-## count words and email totals
 from glob import glob
-
+import matplotlib.pylab as plt
 import os
+import math
+import decimal
 
 ## file paths
 hamPath = 'HamSpam/ham/'
 hamFiles = glob( hamPath + '*.words')
 spamPath = 'HamSpam/spam/'
 spamFiles = glob(spamPath + '*.words')
+testPath = 'HamSpam/test/'
+testFiles = glob(testPath + '*.words')
 
 
 ## Hyperparamaters
 
-alpha = 0.005
+alpha = 0.001
+
+## Vocab total
+vocab = 8000
 
 ## spam and ham counts
 hamWords = dict()
 hamWordsCount = 0
+hamEmailCount = 0
 
 spamWords = dict()
 spamWordsCount = 0
-
+spamEmailCount = 0
 ## Calculate ham words total and counts of each
 for file in hamFiles:
     f = open(file, "r")
     read = f.readlines()
+    hamEmailCount += 1
     for word in read:
         word = word.strip()
 
@@ -38,6 +45,7 @@ for file in hamFiles:
 for file in spamFiles:
     f = open(file, "r")
     read = f.readlines()
+    spamEmailCount += 1
     for word in read:
         word = word.strip()
 
@@ -47,4 +55,46 @@ for file in spamFiles:
             spamWords[word] = 1
             spamWordsCount +=1
 
-print(spamWordsCount)
+h_total = 0
+p_total = 0
+## smoothing - remember to take the log of each
+for val in hamWords:
+    hamWords[val] = hamWords[val] + alpha / (hamWordsCount + (alpha * vocab))
+    hamWords[val] = math.log(hamWords[val])
+    h_total += hamWords[val]
+    
+
+for val in spamWords:
+    spamWords[val] = spamWords[val] + alpha / (spamWordsCount + (alpha * vocab))
+    spamWords[val] = math.log(spamWords[val])
+    p_total += spamWords[val]
+
+spamProb = spamEmailCount / (spamEmailCount + hamEmailCount)
+hamProb = hamEmailCount / (spamEmailCount + hamEmailCount)
+
+
+## test model?
+## issues with normalization/
+## id on test file doesnt work
+count = 1
+for file in testFiles:
+    f = open(file, 'r')
+    read = f.readlines()
+    pspam  = spamProb
+    pham = hamProb
+    for word in read:
+        word = word.strip()
+        if word in hamWords:
+            pham *= hamWords[word]
+        if word in spamWords:
+            pspam *= spamWords[word]
+    if pspam > pham:
+        print(str(count) + ":spam")
+    else:
+        print(str(count) + ":ham")
+    count += 1
+
+
+        
+
+
